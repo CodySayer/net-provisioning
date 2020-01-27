98be66100796f36ee411f2487e700fb9157581d3
Cody Sayer <codymathewsayer@gmail.com>
2020-01-26 18:50:48 -0800

    add final files for assignment 1

--- assignment1/vm_setup.sh
+++ assignment1/vm_setup.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
-install_packages () {
-    sudo yum install git
-    sudo yum install nodejs npm -y
-    sudo yum install mongodb-server npm -y
-    sudo systemctl enable mongod && systemctl start mongod
-}
-
-create_user () {
-    sudo useradd todoapp
-    echo "P@ssw0rd" | sudo passwd --stdin todoapp
-    sudo su - todoapp
-    git clone https://github.com/timoguic/ACIT4640-todo-app.git /home/todo-app/app/
-}
-
 echo "Starting script..."
 
-echo "P@ssw0rd" | scp -P 12022 -r assignment1 admin@localhost
-
-install_packages
-create_user
+scp todoapp -r ./assignment1 admin@localhost:/home/admin
+ssh todoapp '/bin/bash /home/admin/assignment1'
 
 echo "DONE!"
