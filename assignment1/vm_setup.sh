0e3adbf438c589d48c69bfa20cf85398c3ceacb8
Cody Sayer <codymathewsayer@gmail.com>
2020-02-04 08:59:16 -0800

    add almost working vm_setup.sh

--- assignment1/vm_setup.sh
+++ assignment1/vm_setup.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-install_packages () {
-    sudo yum install git
-    sudo yum install nodejs npm -y
-    sudo yum install mongodb-server npm -y
-    sudo systemctl enable mongod && systemctl start mongod
-}
+echo P@ssw0rd | ssh root@localhost -P 12022
 
-create_user () {
-    sudo useradd todoapp
-    echo "P@ssw0rd" | sudo passwd --stdin todoapp
-    sudo su - todoapp
-    git clone https://github.com/timoguic/ACIT4640-todo-app.git /home/todo-app/app/
-}
 
-echo "Starting script..."
+# vm_connect () {
+#     ssh admin@todoapp
+# }
 
-echo "P@ssw0rd" | scp -P 12022 -r assignment1 admin@localhost
+# install_packages () {
+#     sudo yum install git
+#     sudo yum install nodejs npm -y
+#     sudo yum install mongodb-server npm -y
+#     sudo systemctl enable mongod && systemctl start mongod
+# }
 
-install_packages
-create_user
+# create_user () {
+#     sudo useradd todoapp
+#     echo "P@ssw0rd" | sudo passwd --stdin todoapp
+#     sudo su - todoapp
+#     git clone https://github.com/timoguic/ACIT4640-todo-app.git /home/todo-app/app/
+# }
 
-echo "DONE!"
+# echo "Starting script..."
+
+# echo "P@ssw0rd" | scp -P 12022 -r assignment1 admin@localhost
+
+# install_packages
+# create_user
+
+# echo "DONE!"
