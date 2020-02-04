dbeb42a3bf0e6cb8b1e61835e949550b44afafcc
Cody Sayer <codymathewsayer@gmail.com>
2020-02-04 10:03:01 -0800

    add vbox_setup.sh to setup and vm_setup.sh

--- assignment1/vm_setup.sh
+++ assignment1/vm_setup.sh
@@ -2,7 +2,11 @@
 
 echo "Starting script..."
 
-scp -r setup todoapp:/home/admin
+echo "Running vbox_setup.sh"
+/bin/bash ./setup/vbox_setup.sh
+echo "Copying ./setup directory into VM"
+scp -r ./setup todoapp:/home/admin
+echo "Running install script"
 cat install_script.sh | ssh todoapp '/bin/bash /home/admin/setup'
 
 echo "DONE!"
